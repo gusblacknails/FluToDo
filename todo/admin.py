6e7b8f187ac25a2f6b28157e74844695627a5645
Gus Escola <camagrock@gmail.com>
2021-06-07 18:49:25 +0200

    working on delete view

--- todo/admin.py
+++ todo/admin.py
@@ -4,11 +4,12 @@ from .models import Tasks
 
 
 class TasksAdmin(admin.ModelAdmin):
-    
+
     list_display = ['task_name', 'is_completed', 'date_of_creation']
     list_filter = ['is_completed']
 
     class Meta:
         model = Tasks
+
 
 admin.site.register(Tasks, TasksAdmin)
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.edit import DeleteView
+
 from .models import Tasks
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -18,16 +20,34 @@ class FluTodoLoginView(LoginView):
     redirect_authenticated_user = True
 
 
-class TasksListView(LoginRequiredMixin, ListView):
+class TaskListView(LoginRequiredMixin, ListView):
     model = Tasks
     context_object_name = "tasks_list"
 
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+    def post(self, request):
+        input_value = request.POST.get('input')
+        input_id = request.POST.get('id')
+        current_object = self.model.objects.get(
+            id=input_id)
+        if input_value == "True":
+            current_object.is_completed = False
+        else:
+            current_object.is_completed = True
+        current_object.save()
+        context = { "tasks_list": self.model.objects.filter(user=self.request.user) }
+        return render(request, 'todo/tasks_list.html', context)
+
+class TaskDeleteView(DeleteView):
+    model = Tasks
+    success_url = reverse_lazy('todo/tasks_list.html')
 
 def new_user(request):
     # Creamos el formulario de autenticación vacío
     form = NewUserForm()
-    # print("FORM:", form)
-
+  
     def unused_email(email):
         if User.objects.filter(email=email).exists():
             return False
@@ -52,7 +72,6 @@ def new_user(request):
 
                 # Si el usuario se crea correctamente
                 if user is not None:
-                    print("USER IS NOT NONE")
                     # Hacemos el login manualmente
                     login(request, user)
                     # Y le redireccionamos a la portada
